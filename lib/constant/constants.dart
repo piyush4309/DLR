de6cd8027b925c5914e28fea46159692704864a7
Piyush Pilania <piyushpilania@piyush.local>
2022-12-14 16:30:01 +0530

    pushing files

---
+++ lib/constant/constants.dart
@@ -0,0 +1,13 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGIyZDEyOWMzNjFiNUY4MjkyQzZFNDdiRUM4QzUxMTBmZmI4MTQ5RjMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY3MDY3NzM3NTU0NSwibmFtZSI6ImtleV8xIn0.3QcS7Wy5ilzAnqfJ6QPJKxb2TgzOnX9hNftcmR5HAbM';
+
+const String rpcUrl =
+    // "https://rpc-mumbai.maticvigil.com/v1/a5be973518c173bacd9be16a6314dd08b6abcd23";
+"http://127.0.0.1:7545";
+
+const String contractAddress = "0xe9dDE38504705db87c043e1b1aFb8F637E0Acc64";
+
+const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
